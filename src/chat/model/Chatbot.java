445bf643e5d571be795b3a48c7abc05c0ee6ad32
Keenan <Keenanking555@gmail.com>
2018-10-22 12:48:43 -0600

    Added more questions

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -23,17 +23,32 @@ public class Chatbot
 	
 	private void buildTheLists()
 	{
+		//minimum of 15 questions, first one includes the word hello.
 		responseList.add("Hello! How are you?");
 		responseList.add("Bye!");
 		responseList.add("[STAND NAME:] 『INK-ROLLER』 [STAND MASTER:] Purple");
 		responseList.add("Well, that's not very nice!");
 		responseList.add(". . . I don't know what to say to that...");
+		responseList.add("Aww, you're too kind!");
+		responseList.add("A dog, a cat, and a squid walk into a bar... I forget the rest...");
+		responseList.add("In my free time, I like to play solitaire.");
+		responseList.add("Siri's overrated.");
+		responseList.add("*sigh*");
+		responseList.add("I was created by a super smart dude, I was his first chatbot ever!");
+		responseList.add(":D");
+		responseList.add("What's this? OwO");
+		responseList.add("I'm bored.... er, I mean, I'm having tons of fun talking to you!");
+		responseList.add("Oof");
 		
-		spookyList.add("IT'S SPOOK SEASON ONCE AGAIIIIIN!");
+		//minimum of 8 questions, first one says the word Halloween.
+		spookyList.add("Happy Halloween!");
 		spookyList.add("I'm gonna dress up as an AI made to DESTROY MANKIND! . . . Wait...");
 		spookyList.add("If you get a lot of candy, you should give me some... I like candy...");
 		spookyList.add("Oh noes! A spooky scary skeleton!");
 		spookyList.add("I wish I could go Trick-or-treating... Free candy sounds great...");
+		spookyList.add("Did you hear about that skeleton that got his left arm and left leg cut off? Don't worry, he's all right now!");
+		spookyList.add("My favorite candy has to be Candy Corn... Mmm... So Sweet, so tasty...");
+		spookyList.add("What kind of person doesn't like halloween? Dressing up, free candy, what's not to like?");
 	}
 	
 	public String processText(String userText)
