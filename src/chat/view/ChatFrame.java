28e5825168359ae16027ac55945a6d296bd341d1
Keenan <Keenanking555@gmail.com>
2018-12-05 11:58:32 -0700

    Added more buttons, and setup ScrollPane

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,11 +1,28 @@
 package chat.view;
 
-import chat.controller.chatbotController;
 import javax.swing.*;
-
-
+import chat.controller.chatbotController;
 
 public class ChatFrame extends JFrame
 {
-
+	private chatbotController appController;
+	private ChatPanel appPanel;
+	
+	public ChatFrame(chatbotController appController)
+{
+		super();
+		this.appController = appController;
+		this.appPanel = appPanel;
+		setupFrame();
+	}
+	
+		private void setupFrame()
+		{
+			this.setContentPane(appPanel);
+			this.setSize(800, 800);
+			this.setTitle("My first GUI");
+			this.setResizable(true);
+			this.setVisible(true);
+	}
+	
 }
--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -9,9 +9,12 @@ public class ChatPanel extends JPanel
 	private chatbotController appController;
 	private SpringLayout appLayout;
 	private JButton chatButton;
-	private JTextField inputField;
+	private JTextField chatField;
 	private JTextArea chatArea;
 	private JScrollPane chatPane;
+	private JButton checkerButton;
+	private JButton loadButton;
+	private JButton saveButton;
 	
 	public ChatPanel(chatbotController appController)
 	{
@@ -19,10 +22,46 @@ public class ChatPanel extends JPanel
 		
 		this.appController = appController;
 		appLayout = new SpringLayout();
-		chatButton = new JButton("BUTTON!");
-		chatArea = new JTextArea();
+		
+		saveButton = new JButton("Save");
+		loadButton = new JButton("Load");
+		chatButton = new JButton("Chat");
+		checkerButton = new JButton("Check Text");
+		
+		chatField = new JTextField("Talk to the bot here", 50);
+		chatArea = new JTextArea("Chat Area", 20, 50);
 		chatPane = new JScrollPane();
-
+		
+		setupScrollPane();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	private void setupScrollPane()
+	{
+		chatArea.setEditable(false);
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+		
+		chatPane.setViewportView(chatArea);
+		chatPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		chatPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 	}
 	
 }
